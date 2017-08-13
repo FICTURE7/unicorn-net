9c1e7ac6dabc8116dfc78893b8a4c75dd0a19350
FICTURE7 <FICTURE7@gmail.com>
2017-08-13 14:30:20 +0400

    Added TestInvalidMemoryWrite

--- samples/Unicorn.Net.Samples.x86/Program.cs
+++ samples/Unicorn.Net.Samples.x86/Program.cs
@@ -54,6 +54,62 @@ namespace Unicorn.Net.Samples.x86
             }
         }
 
+        public static void TestInvalidMemoryWrite()
+        {
+            var addr = 0x1000000UL;
+            var code = new byte[]
+            {
+                0x89, 0x0D, 0xAA, 0xAA, 0xAA, 0xAA, // MOV [0xaaaaaaaa] ecx
+                0x41, // INC ecx
+                0x4A // DEC edx
+            };
+
+            Console.WriteLine("===================================");
+            Console.WriteLine("Emulate i386 code that write to invalid memory.");
+
+            using (var emulator = new x86Emulator(x86Mode.b32))
+            {
+                // Map 2MB of memory.
+                emulator.Memory.Map(addr, 2 * 1024 * 1024, MemoryPermissions.All);
+                // Write machine code to be emulated.
+                emulator.Memory.Write(addr, code, code.Length);
+
+                // Initialize registers.
+                emulator.Registers.ECX = 0x1234;
+                emulator.Registers.EDX = 0x7890;
+
+                emulator.Hooks.Code.Add(CodeHook, null);
+                emulator.Hooks.Memory.Add(MemoryEventHookType.UnmappedRead | MemoryEventHookType.UnmappedWrite, InvalidMemoryHook, 1, 0, null);
+
+                // Start emulating the machine written machine code.
+                emulator.Start(addr, addr + (ulong)code.Length);
+
+                Console.WriteLine(">>> Emulation done. Below is the CPU context");
+                Console.WriteLine($">>> ECX = 0x{emulator.Registers.ECX.ToString("x2")}");
+                Console.WriteLine($">>> EDX = 0x{emulator.Registers.EDX.ToString("x2")}");
+
+                // Read from memory.
+                var buffer = new byte[4];
+                var tmp = 0;
+
+                emulator.Memory.Read(0xAAAAAAAA, buffer, buffer.Length);
+                tmp = BitConverter.ToInt32(buffer, 0);
+
+                Console.WriteLine($">>> Read 4 bytes from [0x{0xAAAAAAAA.ToString("x2")}] = 0x{tmp.ToString("x2")}");
+
+                try
+                {
+                    emulator.Memory.Read(0xFFFFFFAA, buffer, buffer.Length);
+                    tmp = BitConverter.ToInt32(buffer, 0);
+                    Console.WriteLine($">>> Read 4 bytes from [0x{0xFFFFFFAA.ToString("x2")}] = 0x{tmp.ToString("x2")}");
+                }
+                catch
+                {
+                    Console.WriteLine($">>> Failed to read 4 bytes from [0x{0xFFFFFFAA.ToString("x2")}]");
+                }
+            }
+        }
+
         // test_i386_loop
         public static void TestLoop()
         {
@@ -97,9 +153,26 @@ namespace Unicorn.Net.Samples.x86
             Console.WriteLine($">>> --- EFLAGS is {eflags.ToString("x2")}");
         }
 
+        private static bool InvalidMemoryHook(Emulator emulator, MemoryType type, ulong address, int size, ulong value, object userData)
+        {
+            switch (type)
+            {
+                case MemoryType.WriteUnmapped:
+                    Console.WriteLine($">>> Missing memory is being WRITE at 0x{address.ToString("x2")}, data size = {size}, data value = 0x{value.ToString("x2")}");
+
+                    // Map missing memory & return true to tell unicorn we want to continue execution.
+                    emulator.Memory.Map(0xAAAA0000, 2 * 1024 * 1024, MemoryPermissions.All);
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+
         public static void Main(string[] args)
         {
             TestInvalidMemoryRead();
+            TestInvalidMemoryWrite();
             TestLoop();
 
             Console.ReadLine();
