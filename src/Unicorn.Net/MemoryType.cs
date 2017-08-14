a8bee9fc83bc50026fb95f8e0c53048fd341955c
FICTURE7 <FICTURE7@gmail.com>
2017-08-14 18:32:55 +0400

    Fixed typo & added docs

--- src/Unicorn.Net/MemoryType.cs
+++ src/Unicorn.Net/MemoryType.cs
@@ -1,19 +1,54 @@
-﻿namespace Unicorn
+﻿using Unicorn.Internal;
+
+namespace Unicorn
 {
+    /// <summary>
+    /// Types of memory accesses for memory hooks.
+    /// </summary>
     public enum MemoryType
     {
-        Read = 16,
-        Write,
-        Fetch,
+        /// <summary>
+        /// Memory read from.
+        /// </summary>
+        Read = uc_mem_type.UC_MEM_READ,
+        /// <summary>
+        /// Memory written to.
+        /// </summary>
+        Write = uc_mem_type.UC_MEM_WRITE,
+        /// <summary>
+        /// Memory fetched at.
+        /// </summary>
+        Fetch = uc_mem_type.UC_MEM_FETCH,
 
-        ReadUnmapped,
-        WriteUnmapped,
-        FetchUnmapped,
+        /// <summary>
+        /// Unmapped memory read from.
+        /// </summary>
+        ReadUnmapped = uc_mem_type.UC_MEM_READ_UNMAPPED,
+        /// <summary>
+        /// Unmapped memory written to.
+        /// </summary>
+        WriteUnmapped = uc_mem_type.UC_MEM_WRITE_UNMAPPED, 
+        /// <summary>
+        /// Unmapped memory fetched at.
+        /// </summary>
+        FetchUnmapped = uc_mem_type.UC_MEM_FETCH_UNMAPPED,
 
-        ReadProtected,
-        WriteProceted,
-        FetchProtected,
+        /// <summary>
+        /// Write to write protected memory.
+        /// </summary>
+        WriteProtected = uc_mem_type.UC_MEM_WRITE_PROT,
+        /// <summary>
+        /// Read to read protected memory.
+        /// </summary>
+        ReadProtected = uc_mem_type.UC_MEM_READ_PROT,
+        /// <summary>
+        /// Fetch on non-executable memory.
+        /// </summary>
+        FetchProtected = uc_mem_type.UC_MEM_FETCH_PROT,
 
-        ReadAfter
+        /// <summary>
+        /// Successful memory read.
+        /// </summary>
+        ReadAfter = uc_mem_type.UC_MEM_READ_AFTER
     }
 }
