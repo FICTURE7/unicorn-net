1d9a711bf5b192bfd37416ebe382bf81cbfaa560
FICTURE7 <FICTURE7@gmail.com>
2017-10-07 15:30:32 +0400

    Fixed gen_reg_properties.py

--- scripts/gen_reg_properties.py
+++ scripts/gen_reg_properties.py
@@ -1,10 +1,13 @@
 import sys
+import os
 
 def main(args):
-    if len(args) < 2:
-        print("Must specify file defining registers in command line argument.", file=sys.stderr)
+    if len(args) < 3:
+        print("ERROR: Must specify file defining registers and prefix of register names in command line argument.", file=sys.stderr)
+        print(os.path.basename(__file__) + " <filename> <register prefix>");
         exit(1)
-
+    
+    regs_prefix = args[2] + '_'
     regs_file = open(args[1], 'r')
     regs = regs_file.read().split(',')
     regs_file.close()
@@ -13,10 +16,10 @@ def main(args):
     final_code = ''
     for reg in regs:
         reg = reg.strip()
-
-        name = reg[reg.rindex('_') + 1:]
+		
+        name = reg[reg.index(regs_prefix) + len(regs_prefix):]
         code = "/// <summary>\n/// Gets or sets the value of {0} register.\n/// </summary>\n".format(name)
-        code += "public long {0} \n{\n".format(name)
+        code += "public long {0} \n{{\n".format(name)
         code += "    get {{ return Read({0}); }}\n".format(regid)
         code += "    set {{ Write({0}, value); }}".format(regid)
         code += "\n}\n\n";
