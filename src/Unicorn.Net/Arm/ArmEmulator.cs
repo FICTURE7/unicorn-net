c32973f276f958aec78ac966165752bb526102c9
FICTURE7 <FICTURE7@gmail.com>
2017-10-07 15:30:56 +0400

    Implementation of ArmEmulator

---
+++ src/Unicorn.Net/Arm/ArmEmulator.cs
@@ -0,0 +1,46 @@
+﻿namespace Unicorn.Arm
+{
+    /// <summary>
+    /// Represents an ARM architecture <see cref="Emulator"/>.
+    /// </summary>
+    public class ArmEmulator : Emulator
+    {
+        private readonly ArmRegisters _registers;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArmEmulator"/> class with the specified
+        /// <see cref="ArmMode"/> to use.
+        /// </summary>
+        /// <param name="mode">Mode to use.</param>
+        public ArmEmulator(ArmMode mode) : base(Bindings.Arch.ARM, (Bindings.Mode)mode)
+        {
+            _registers = new ArmRegisters(this);
+        }
+
+        /// <summary>
+        /// Gets the <see cref="ArmRegisters"/> of the <see cref="ArmEmulator"/> instance.
+        /// </summary>
+        public ArmRegisters Registers
+        {
+            get
+            {
+                CheckDisposed();
+
+                return _registers;
+            }
+        }
+
+        /// <summary>
+        /// Gets the <see cref="ArmMode"/> of the <see cref="ArmEmulator"/>.
+        /// </summary>
+        public ArmMode Mode
+        {
+            get
+            {
+                CheckDisposed();
+
+                return (ArmMode)_mode;
+            }
+        }
+    }
+}
---
+++ src/Unicorn.Net/Arm/ArmMode.cs
@@ -0,0 +1,23 @@
+﻿namespace Unicorn.Arm
+{
+    /// <summary>
+    /// Defines the modes of an <see cref="ArmEmulator"/>.
+    /// </summary>
+    public enum ArmMode
+    {
+        /// <summary>
+        /// Thumb mode.
+        /// </summary>
+        Thumb = Bindings.Mode.ARMThumb,
+
+        /// <summary>
+        /// Cortext-M series mode.
+        /// </summary>
+        MClass = Bindings.Mode.ARMMClass,
+
+        /// <summary>
+        /// ARMv8 A32 encoding mode.
+        /// </summary>
+        v8 = Bindings.Mode.ARMv8
+    }
+}
